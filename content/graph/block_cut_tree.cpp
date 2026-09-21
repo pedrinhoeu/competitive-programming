c1f9c4cbdc7b47818430cb9b965957043b4d15b3
pedro_Simoes <pelusimoes3@gmail.com>
2026-09-21 19:46:40 -0300

    fix: block_cut tree on non connected graphs

--- content/graph/block_cut_tree.cpp
+++ content/graph/block_cut_tree.cpp
@@ -1,24 +1,25 @@
 #include "../contest/template.cpp"
 
-vector<vector<int>> block_cut(vector<vector<int>> g, vector<int> &is_cut, vector<int> &at) {
+vector<vector<int>> block_cut(vector<vector<int>> g, vector<int> &is_cut, vector<int> &at, vector<vector<int>> &comps) {
     int n = sz(g);
     is_cut.resize(n);
 
     vector<int> st, tin(n), low(n);
-    vector<vector<int>> comps;
 
     int t = 0;
     auto dfs = [&](auto &self, int v, int p) -> void {
         tin[v] = low[v] = ++t;
         st.push_back(v);
+        int children = 0;
         for (int ch : g[v]) if (ch != p) {
             if (tin[ch]) {
                 low[v] = min(low[v], tin[ch]);
             } else {
+                children++;
                 self(self, ch, v);
                 low[v] = min(low[v], low[ch]);
                 if (low[ch] >= tin[v]) {
-                    is_cut[v] |= tin[v] > 1 || tin[ch] > 2;
+                    if (p != -1 || children > 1) is_cut[v] = 1;
                     comps.push_back({v});
                     while (comps.back().back() != ch) {
                         comps.back().push_back(st.back());
@@ -28,7 +29,7 @@ vector<vector<int>> block_cut(vector<vector<int>> g, vector<int> &is_cut, vector
             }
         }
     };
-    dfs(dfs, 0, -1);
+    rep(i, 0, n) if (!tin[i]) dfs(dfs, i, -1);
 
     at.resize(n);
     vector<vector<int>> adj;
